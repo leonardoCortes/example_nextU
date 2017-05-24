9dc1121034dbf2c9d0834a0ca0ee609c05a35092
Leonardo Fabian Cortes Vasquez <ingeniero.lfcortes@gmail.com>
2017-05-24 10:05:42 -0500

    Initial Commit

---
+++ HolaMundo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HolaMundo2
+//
+//  Created by Leonardo Fabian Cortes Vasquez on 24/05/17.
+//  Copyright © 2017 EjercicioNextU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
